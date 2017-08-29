c90e9ee74f065b9762090239f6aa7d4f18a4b912
Dylan Lawless (CA Account) <ca_K1517669@kclad.ds.kcl.ac.uk>
2017-08-29 16:15:56 +0100

    Initial commit

---
+++ app/src/main/java/com/souvlakistreet/souvlakistreet/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.souvlakistreet.souvlakistreet;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ca_k1517669 on 29/08/2017.
+ */
+
+public class HomeFragment extends Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView=inflater.inflate(R.layout.home_layout, container, false);
+        return myView;
+    }
+}
